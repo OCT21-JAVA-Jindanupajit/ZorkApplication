7bbd87aebb4e57c5399d0503024622c32c614545
fenglin098 <feng.lin098@gmail.com>
2019-10-28 10:59:14 -0400

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,53 @@
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-
+        Scanner keyboard=new Scanner(System.in);
+        String userInput;
         ArrayList<Room> roomCollection;
         int counter= 0;
         Room currentRoom;
+        boolean quitRequested = false;
+
+        do {
+            System.out.print("Which direction you want to go [ N, S, E, W] ? ");
+            userInput = keyboard.nextLine().trim().toUpperCase();
+            switch(userInput){
+                case "N":
+                    move(roomCollection, currentRoom);
+                    break;
+                case "S":
+                    move(roomCollection, currentRoom);
+                    break;
+                case "E":
+                    move(roomCollection, currentRoom);
+                    break;
+                case "W":
+                    move(roomCollection, currentRoom);
+                    break;
+                case "Q":
+                    quitRequested = true;
+                    break;
+                default:
+                    System.out.println("Enter a valid direction.");
+            }
 
 
 
+
+
+        } while (!quitRequested);
+
+        quit(counter);
 
 
     }
 
 
-    private static int move(HahMap<> map, int currentRoom, String userRequest) {
+    private static int move(ArrayList<Room> roomCollection, Room currentRoom) {
 
     }
 
