566f767a8c8de7060440649d54e02adc92fc24c8
fenglin098 <feng.lin098@gmail.com>
2019-10-28 11:28:06 -0400

    Data Structures

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,18 @@ public class Main {
     public static void main(String[] args){
         Scanner keyboard=new Scanner(System.in);
         String userInput;
-        ArrayList<Room> roomCollection;
         int counter= 0;
-        Room currentRoom;
         boolean quitRequested = false;
+
+        /* Room currentRoom = null;ArrayList<Room> roomCollection = null; */
+
+        HashMap<Integer, String> roomName;
+        HashMap<Integer, Boolean> roomLocked;
+        HashMap<Integer, ArrayList<String>> roomItemCollection;
+        HashMap<Integer, HashMap<String, Integer>> roomDoorCollection;
+
+        init(roomName, roomLocked, roomItemCollection, roomDoorCollection);
+
 
         do {
             System.out.print("Which direction you want to go [ N, S, E, W] ? ");
@@ -45,10 +53,44 @@ public class Main {
 
 
     }
+    private static void init(
+        HashMap<Integer, String> roomName,
+        HashMap<Integer, Boolean> roomLocked,
+        HashMap<Integer, ArrayList<String>> roomItemCollection,
+        HashMap<Integer, HashMap<String, Integer>> roomDoorCollection) {
+
+        roomName.put(1,"foyer");
+        roomLocked.put(1,false);
+
+        roomName.put(2,"front room");
+        roomLocked.put(2,false);
+
+
+        roomName.put(3,"library");
+        roomLocked.put(3,false);
+
+        roomName.put(4,"kitchen");
+        roomLocked.put(4,false);
+
+        roomName.put(5,"dining room");
+        roomLocked.put(5,false);
+
+        roomName.put(6,"vault");
+        roomLocked.put(6,false);
+
+        roomName.put(7,"parlor");
+        roomLocked.put(7,false);
+
+        roomName.put(8,"secret room");
+        roomLocked.put(8,true);
+
+    }
+
 
 
     private static int move(ArrayList<Room> roomCollection, Room currentRoom) {
 
+        return 0;
     }
 
     private static void quit(int counter) {
@@ -58,15 +100,20 @@ public class Main {
 }
 
 
-class Room {
-    int number;
-    String name;
-    ArrayList<String> itemCollection;
-    ArrayList<Door> doorCollection;
-}
 
-class Door {
-    String direction; /* N S E W X */
-    Room nextRoom;
-    boolean isOpen = true;
-}
+
+
+
+
+//class Room {
+//    int number;
+//    String name;
+//    ArrayList<String> itemCollection;
+//    ArrayList<Door> doorCollection;
+//}
+//
+//class Door {
+//    String direction; /* N S E W X */
+//    Room nextRoom;
+//    boolean isOpen = true;
+//}
