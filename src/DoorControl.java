e3d7e48f785aa329ac0b27a42e54a0421514209d
kcheung3102 <kcheung3102@gmail.com>
2019-10-28 14:59:38 -0400

    doorfeat

--- src/DoorControl.java
+++ src/DoorControl.java
@@ -2,6 +2,14 @@ public class DoorControl {
 
     public static int moveThrough(int currentRoom, String direction) {
         int newRoom = currentRoom;
+            if(newRoom == null) {
+                System.out.println("error, you can't go this direction");
+            }
+
+
+
+
+
 
 
 
