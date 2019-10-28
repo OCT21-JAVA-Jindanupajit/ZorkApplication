21e8be9311140c00a1975e3b35944ad30f61320d
kcheung3102 <kcheung3102@gmail.com>
2019-10-28 15:34:31 -0400

    DoorControl

--- src/DoorControl.java
+++ src/DoorControl.java
@@ -1,9 +1,14 @@
 public class DoorControl {
 
     public static int moveThrough(int currentRoom, String direction) {
-
+        int nextRoom = currentRoom;
+        String roomName;
         if(ValidateDirection.IsValid(currentRoom,direction)) {
-
+            nextRoom = Data.roomDoorCollection.get(currentRoom)
+                    .get(direction);
+            roomName = Data.roomName.get(nextRoom);
+            System.out.println("You are in " + roomName);
+            System.out.println(nextRoom);
         }
 
 
@@ -15,7 +20,8 @@ public class DoorControl {
 
 
 
-        return currentRoom;
+
+        return nextRoom;
     }
 
 }
