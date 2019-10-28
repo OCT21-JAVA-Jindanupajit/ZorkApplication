8c72e45a9db2d91b81fa16ee1474380bea4a0b4f
fenglin098 <feng.lin098@gmail.com>
2019-10-28 14:41:57 -0400

    ghost

--- src/Ghost.java
+++ src/Ghost.java
@@ -1,12 +1,14 @@
+import java.util.Random;
+
 public class Ghost {
 
     public static boolean FollowByGhost() {
         boolean isFollowByGhost = false;
-
-
-
-
-
+        Random r=new Random();
+        int ghost=1+r.nextInt(4);
+        if (ghost==1){
+            isFollowByGhost=true;
+        }
 
         return isFollowByGhost;
     }
